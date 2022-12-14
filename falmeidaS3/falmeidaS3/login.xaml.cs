c0cb3e9fd9b5867062b1904c5788885c9ce209ee
Fausto <Fausto@DESKTOP-2G47DPC>
2022-12-13 19:32:09 -0500

    Agregar archivos de proyecto.

---
+++ falmeidaS3/falmeidaS3/login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace falmeidaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "falmeida";
+            string clave = "12345";
+            if(usuario==txtUsuario.Text && clave == txtClave.Text) {
+                Navigation.PushAsync(new registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contrasela Incorrecto", "Cerrar");
+            }
+                
+        }
+    }
+}
---
+++ falmeidaS3/falmeidaS3/registro.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace falmeidaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+    }
+}
